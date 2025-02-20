9a9e0f8babfd0445e36cacb2e9b7c8798d7009b6
RalphDev <108262757+Rannh02@users.noreply.github.com>
2025-02-21 01:43:21 +0800

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,16 @@
+package gui_System;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+	
+	public Frame() {
+		this.setTitle("MoneyConverter");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(400,400);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		
+	}
+}
---
+++ main.java
@@ -0,0 +1,8 @@
+package gui_System;
+
+public class main {
+	public static void main (String[]args) {
+		new Frame();
+	}
+
+}
